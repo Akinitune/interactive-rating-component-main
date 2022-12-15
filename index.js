e8007b341ec7a7cd4685565d3100956db71463b0
akinitune <akinitune05@gmail.com>
2022-12-15 02:16:47 -0600

    message

--- index.js
+++ index.js
@@ -3,21 +3,36 @@
 function gate(){
   
     document.getElementById('hey').style.zIndex= "0";
-    document.getElementById('there').innerHTML = " You did not choose a rate"; 
+    
     
    
 }
 function ratingNumber(nomber){
 switch(nomber){
-    case 1:document.getElementById('there').innerHTML = " You 1 selected  out of 5";
+    case 1:
+        document.getElementById('there').innerHTML = " You 1 selected  out of 5";
+        document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
+        document.getElementById('changing').style.color= "white";
     break;
-    case 2:document.getElementById('there').innerHTML = " You 2 selected  out of 5";
+    case 2:document.getElementById('there').innerHTML = " You 2 selected  out of 5"
+    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changing').style.color= "white";
+    ;
     break;
-    case 3:document.getElementById('there').innerHTML = " You 3 selected  out of 5";
+    case 3:document.getElementById('there').innerHTML = " You 3 selected  out of 5"
+    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changing').style.color= "white";
+    ;
     break;
-    case 4:document.getElementById('there').innerHTML = " You 4 selected  out of 5";
+    case 4:document.getElementById('there').innerHTML = " You 4 selected  out of 5"
+    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changing').style.color= "white";
+    ;
     break;
-    case 5:document.getElementById('there').innerHTML = " You 5 selected  out of 5";
+    case 5:document.getElementById('there').innerHTML = " You 5 selected  out of 5"
+    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changing').style.color= "white";
+    ;
     break;
     default:document.getElementById('there').innerHTML = "you did not choose a rate";
 }
