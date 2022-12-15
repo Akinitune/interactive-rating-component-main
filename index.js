addf285109f29514792db48b34c25668873f5632
akinitune <akinitune05@gmail.com>
2022-12-15 02:26:52 -0600

    message

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ switch(nomber){
         document.getElementById('changing').style.color= "white";
     break;
     case 2:document.getElementById('there').innerHTML = " You 2 selected  out of 5"
-    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
-    document.getElementById('changing').style.color= "white";
+    document.getElementById('changingtwo').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changingtwo').style.color= "white";
     ;
     break;
     case 3:document.getElementById('there').innerHTML = " You 3 selected  out of 5"
-    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
-    document.getElementById('changing').style.color= "white";
+    document.getElementById('changingthree').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changingthree').style.color= "white";
     ;
     break;
     case 4:document.getElementById('there').innerHTML = " You 4 selected  out of 5"
-    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
-    document.getElementById('changing').style.color= "white";
+    document.getElementById('changingfour').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changingfour').style.color= "white";
     ;
     break;
     case 5:document.getElementById('there').innerHTML = " You 5 selected  out of 5"
-    document.getElementById('changing').style.backgroundColor= "hsl(217, 12%, 63%)";
-    document.getElementById('changing').style.color= "white";
+    document.getElementById('changingfive').style.backgroundColor= "hsl(217, 12%, 63%)";
+    document.getElementById('changingfive').style.color= "white";
     ;
     break;
     default:document.getElementById('there').innerHTML = "you did not choose a rate";
